afe422e3319bc6bdd4fa0deca45fd889379f8d54
Jason Gonzales <jasongonzo25@gmail.com>
2020-09-22 15:22:57 -0700

    completed add a grade

--- components/app.js
+++ components/app.js
@@ -1,15 +1,19 @@
 class App {
-  constructor(gradeTable,pageHeader) {
-    // this.pageHeaader = pageHeaader;
+  constructor(gradeTable,pageHeader,gradeForm) {
     this.gradeTable = gradeTable;
     this.pageHeader = pageHeader;
+    this.gradeForm = gradeForm;
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
-  }
+    this.createGrade = this.createGrade.bind(this);
+    this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
+    this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
+}
   handleGetGradesError(error) {
     console.error(error);
   }
   handleGetGradesSuccess(grades) {
+
     this.gradeTable.updateGrades(grades);
     var sum = 0;
 
@@ -35,8 +39,35 @@ class App {
 
     })
   }
-
   start() {
+    this.getGrades();
+    this.gradeForm.onSubmit(this.createGrade);
+
+  }
+  createGrade(name,course,grade) {
+    console.log(name,course,grade);
+    $.ajax({
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      method: "POST",
+      data: {
+        "name":name,
+        "course":course,
+        "grade":grade
+      },
+      headers: {
+        "X-Access-Token":"nsXceUhs"
+      },
+      sucess:this.handleCreateGradeSuccess,
+      error:this.handleCreateGradeError
+    });
+
+  }
+
+  handleCreateGradeError(error) {
+    console.error(error);
+  }
+
+  handleCreateGradeSuccess() {
     this.getGrades();
   }
 }
